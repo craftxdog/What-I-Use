b59fad2e45a365d24523e4836b14d0bbb0f8cbeb
craftxdog <aaronmars77@gmail.com>
2024-08-01 00:16:48 -0600

    fix(meta): update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://craftxdog.io",
   integrations: [
     sitemap(),
     react(),
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "What I Use - Erwing Aaron Ulloa Silva";
+export const SITE_DESCRIPTION = "A curated list of the tech I use";
+export const HOMEPAGE_URL = "https://craftxdog.io";
